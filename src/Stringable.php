09afc44db406d5e6857258a65cfbbb90f0a847a0
Andrey <alestro@bk.ru>
2020-07-02 15:39:32 +0200

    Create Stringable.php

---
+++ src/Stringable.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Bermuda\String;
+
+
+/**
+ * interface Stringable
+ * @package Bermuda\String
+ */
+interface Stringable
+{
+    /**
+     * @return string
+     */
+    public function __toString(): string ;
+}
