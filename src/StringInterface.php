da37cb400ff7f9648bd85a4d642a83058e22f748
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 23:02:08 +0200

    Update StringInterface.php

--- src/StringInterface.php
+++ src/StringInterface.php
@@ -2,31 +2,31 @@
 
 namespace Bermuda\String;
 
+use ArrayAccess;
 use Bermuda\Arrayable;
+use Countable;
+use IteratorAggregate;
+use RuntimeException;
 
-/**
- * interface StringInterface
- * @package Bermuda\String
- */
-interface StringInterface extends \IteratorAggregate, \ArrayAccess, \Countable, Arrayable, Jsonable, Stringable
+interface IString extends IteratorAggregate, ArrayAccess, Countable, Arrayable, Jsonable, Stringable
 {
     /**
      * dump string and die
      */
-    public function dd(): void ;
+    public function dd(): void;
 
     /**
      * @return string
      */
-    public function encoding(): string ;
-    
-    public function slice(int $bytes): StringInterface ;
-   
+    public function encoding(): string;
+
+    public function slice(int $bytes): IString;
+
     /**
      * @param string $encoding
-     * @return StringInterface
+     * @return IString
      */
-    public function encode(string $encoding): StringInterface ;
+    public function encode(string $encoding): IString;
 
     /**
      * @param string $needle
@@ -34,23 +34,23 @@ interface StringInterface extends \IteratorAggregate, \ArrayAccess, \Countable, 
      * @param bool $caseInsensitive
      * @return int|null
      */
-    public function indexOf(string $needle, int $offset = 0, bool $caseInsensitive = false):? int ;
-    
+    public function indexOf(string $needle, int $offset = 0, bool $caseInsensitive = false): ?int;
+
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function copy(): StringInterface ;
+    public function copy(): IString;
 
     /**
      * @param string $delim
-     * @return StringInterface[]|string[]
+     * @return IString[]|string[]
      */
-    public function explode(string $delim = '/', int $limit = PHP_INT_MAX, bool $asString = false): array ;
+    public function explode(string $delim = '/', int $limit = PHP_INT_MAX, bool $asString = false): array;
 
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function ucFirst(): StringInterface ;
+    public function ucFirst(): IString;
 
     /**
      * @param string $needle
@@ -58,178 +58,178 @@ interface StringInterface extends \IteratorAggregate, \ArrayAccess, \Countable, 
      * @param bool $caseInsensitive
      * @return bool
      */
-    public function contains(string $needle, int $offset = 0, bool $caseInsensitive = false): bool ;
+    public function contains(string $needle, int $offset = 0, bool $caseInsensitive = false): bool;
 
     /**
      * @param int $length
      * @param string $substring
-     * @return StringInterface
+     * @return IString
      */
-    public function truncate(int $length = 200, string $substring = '...'): StringInterface ;
+    public function truncate(int $length = 200, string $substring = '...'): IString;
 
     /**
      * @return int
      */
-    public function length(): int ;
-    
+    public function length(): int;
+
     /**
      * @return int
      */
-    public function getBytes(): int ;
+    public function getBytes(): int;
 
     /**
      * @param string $needle
      * @param bool $requireNeedle
      * @param bool $caseInsensitive
-     * @return StringInterface|null
+     * @return IString|null
      */
-    public function before(string $needle, bool $requireNeedle = true, bool $caseInsensitive = false):? StringInterface ; 
+    public function before(string $needle, bool $requireNeedle = true, bool $caseInsensitive = false): ?IString;
 
     /**
      * @param string $needle
      * @param bool $requireNeedle
      * @param bool $caseInsensitive
-     * @return StringInterface|null
+     * @return IString|null
      */
-    public function after(string $needle, bool $requireNeedle = true, bool $caseInsensitive = false):? StringInterface ;
+    public function after(string $needle, bool $requireNeedle = true, bool $caseInsensitive = false): ?IString;
 
     /**
      * @param string $algorithm
      * @return string
      */
-    public function hash(string $algorithm = 'sha512'): string ;
+    public function hash(string $algorithm = 'sha512'): string;
 
     /**
      * @param string $charlist
      * @return Stringy
      */
-    public function trim(string $charlist = ' '): StringInterface ;
+    public function trim(string $charlist = ' '): IString;
 
     /**
      * @param string $charlist
-     * @return StringInterface
+     * @return IString
      */
-    public function ltrim(string $charlist = ' '): StringInterface ;
+    public function ltrim(string $charlist = ' '): IString;
 
     /**
      * @param string $charlist
-     * @return StringInterface
+     * @return IString
      */
-    public function rtrim(string $charlist = ' '): StringInterface ;
+    public function rtrim(string $charlist = ' '): IString;
 
     /**
      * @param string|array $search
      * @param string|array $replace
-     * @return StringInterface
+     * @return IString
      */
-    public function replace($search, $replace): StringInterface ;
+    public function replace($search, $replace): IString;
 
     /**
      * @param string $subject
-     * @return StringInterface
+     * @return IString
      */
-    public function prepend(string $subject): StringInterface ;
+    public function prepend(string $subject): IString;
 
     /**
      * @param string $subject
-     * @return StringInterface
+     * @return IString
      */
-    public function append(string $subject): StringInterface ;
+    public function append(string $subject): IString;
 
     /**
      * @param string $subject
      * @param bool $caseInsensitive
      * @return bool
      */
-    public function equals(string $subject, bool $caseInsensitive = false): bool ;
+    public function equals(string $subject, bool $caseInsensitive = false): bool;
 
     /**
      * @param string[] $subject
      * @param bool $caseInsensitive
      * @return bool
      */
-    public function equalsAny(array $subject, bool $caseInsensitive = false): bool ;
+    public function equalsAny(array $subject, bool $caseInsensitive = false): bool;
 
     /**
      * @return bool
      */
-    public function isEmpty(): bool ;
-    
+    public function isEmpty(): bool;
+
     /**
      * @param int $index
-     * @return StringInterface
-     * @throws \RuntimeException
+     * @return IString
+     * @throws RuntimeException
      */
-    public function index(int $index): StringInterface ;
+    public function index(int $index): IString;
 
     /**
      * @param int $start
      * @param int $end
-     * @return StringInterface
+     * @return IString
      */
-    public function interval(int $start, int $end): StringInterface ; 
+    public function interval(int $start, int $end): IString;
 
     /**
      * @param int $index
      * @return bool
      */
-    public function has(int $index): bool ;
-    
-    /**
-     * @return StringInterface|null
-     */
-    public function first():? StringInterface ;
+    public function has(int $index): bool;
 
     /**
-     * @return StringInterface|null
+     * @return IString|null
      */
-    public function last():? StringINterface ;
+    public function first(): ?IString;
+
+    /**
+     * @return IString|null
+     */
+    public function last(): ?IString;
 
     /**
      * @param string $char
-     * @return StringInterface
+     * @return IString
      */
-    public function wrap(string $char): StringInterface ;
-    
+    public function wrap(string $char): IString;
+
     /**
      * @param string|null $char
      * @return bool
      */
-    public function isWrapped(string $char): bool ;
-    
+    public function isWrapped(string $char): bool;
+
     /**
      * @param int $pos
-     * @return StringInterface[]
+     * @return IString[]
      */
-    public function break(int $pos): array ;
+    public function break(int $pos): array;
 
     /**
      * @param int $pos
      * @param int|null $length
-     * @return StringInterface
+     * @return IString
      */
-    public function substring(int $pos, int $length = null): StringInterface ;
+    public function substring(int $pos, int $length = null): IString;
 
     /**
      * @param int $length
-     * @return StringInterface
+     * @return IString
      */
-    public function start(int $length): StringInterface ; 
+    public function start(int $length): IString;
 
     /**
      * @param int $length
-     * @return StringInterface
+     * @return IString
      */
-    public function end(int $length): StringInterface ;
+    public function end(int $length): IString;
 
     /**
      * @param string|string[] $pattern
      * @param string|string[] $replacement
      * @param int $limit
      * @param int|null $count
-     * @return StringInterface
+     * @return IString
      */
-    public function pregReplace($pattern, $replacement, int $limit = -1, int &$count = null): StringInterface ; 
+    public function pregReplace($pattern, $replacement, int $limit = -1, int &$count = null): IString;
 
     /**
      * @param string $pattern
@@ -238,7 +238,7 @@ interface StringInterface extends \IteratorAggregate, \ArrayAccess, \Countable, 
      * @param int $offset
      * @return bool
      */
-    public function match(string $pattern, ?array &$matches = [], int $flags = 0, int $offset = 0): bool ;
+    public function match(string $pattern, ?array &$matches = [], int $flags = 0, int $offset = 0): bool;
 
     /**
      * @param string $pattern
@@ -247,69 +247,69 @@ interface StringInterface extends \IteratorAggregate, \ArrayAccess, \Countable, 
      * @param int $offset
      * @return bool
      */
-    public function matchAll(string $pattern, ?array &$matches = [], int $flags = 0, int $offset = 0): bool ;
-    
-    /**
-     * @return StringInterface
-     */
-    public function revers(): StringInterface ;
+    public function matchAll(string $pattern, ?array &$matches = [], int $flags = 0, int $offset = 0): bool;
 
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function lcFirst(): StringInterface ;
+    public function revers(): IString;
+
+    /**
+     * @return IString
+     */
+    public function lcFirst(): IString;
 
     /**
      * @param int $len
-     * @return StringInterface
+     * @return IString
      */
-    public function rand(int $len): StringInterface ;
+    public function rand(int $len): IString;
 
     /**
      * Write string
      */
-    public function write(): void ;
+    public function write(): void;
 
     /**
      * @return int
      */
-    public function lastIndex():? int ;
+    public function lastIndex(): ?int;
 
     /**
      * @return int|null
      */
-    public function firstIndex():? int ;
+    public function firstIndex(): ?int;
 
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function shuffle(): StringInterface ;
+    public function shuffle(): IString;
 
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function toUpper(): StringInterface ;
+    public function toUpper(): IString;
 
     /**
-     * @return StringInterface
+     * @return IString
      */
-    public function toLower(): StringInterface ; 
+    public function toLower(): IString;
 
     /**
      * @param int $length
-     * @return StringInterface[]
+     * @return IString[]
      */
-    public function split(int $length = 1): array ;
+    public function split(int $length = 1): array;
 
     /**
      * @param mixed ... $args
-     * @return StringInterface
-     * @throws \RuntimeException
+     * @return IString
+     * @throws RuntimeException
      */
-    public function format(... $args): StringInterface ;
+    public function format(...$args): IString;
 
     /**
      * @return bool
      */
-    public function isJson(): bool ;
+    public function isJson(): bool;
 }
