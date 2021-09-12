3c5222b48b9b1a11e47a2aebe849bb573dde3254
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 23:05:28 +0200

    Update Str.php

--- src/Str.php
+++ src/Str.php
@@ -2,30 +2,15 @@
 
 namespace Bermuda\String;
 
-/**
- * Class Str
- * @package Bermuda\String
- */
+use Exception;
+use finfo;
+use Throwable;
+
 final class Str
 {
-    private function __construct()
-    {
-    }
-
     private const numbers = '0123456789';
     private const symbols = '[~`!@#$%^&*()}{?<>/|_=+-]';
     private const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    /**
-     * @param string $x
-     * @param string $y
-     * @param bool $caseInsensitive
-     * @return bool
-     */
-    public static function equals(string $x, string $y, bool $caseInsensitive = true): bool
-    {
-        return $caseInsensitive ? strcasecmp($x, $y) == 0 : strcmp($x, $y) == 0;
-    }
 
     /**
      * @param string $content
@@ -35,30 +20,36 @@ final class Str
     {
         return self::finfoBuffer(FILEINFO_MIME_TYPE, $content);
     }
-    
-    /**
-     * @param int $length
-     * @return StringInterface
-     */
-    public function slice(int $length): StringInterface
+
+    private static function finfoBuffer(string $flags, string $content): string
     {
-        return $this->substring($length);
+        return (new finfo($flags))->buffer($content);
     }
 
     /**
      * @param string $subject
      * @param string ...$segments
      */
-    public static function prepend(string &$subject, string ... $segments): void
+    public static function prepend(string &$subject, string ...$segments): void
     {
         $subject = self::implode($segments, '') . $subject;
     }
 
     /**
+     * @param string $haystack
+     * @param string $glue
+     * @return string
+     */
+    public static function implode(array $haystack, string $glue = ','): string
+    {
+        return implode($glue, $haystack);
+    }
+
+    /**
      * @param string $subject
      * @param string ...$segments
      */
-    public static function append(string &$subject, string ... $segments): void
+    public static function append(string &$subject, string ...$segments): void
     {
         $subject .= self::implode($segments, '');
     }
@@ -72,11 +63,6 @@ final class Str
         return self::finfoBuffer(FILEINFO_EXTENSION, $content);
     }
 
-    private static function finfoBuffer(string $flags, string $content): string
-    {
-        return (new \finfo($flags))->buffer($content);
-    }
-    
     /**
      * @param string $string
      * @return bool
@@ -85,7 +71,26 @@ final class Str
     {
         return self::match("/<[^<]+>/", $string);
     }
-    
+
+    /**
+     * @param string $regexp
+     * @param string $subject
+     * @return bool
+     */
+    public static function match(string $regexp, string $subject): bool
+    {
+        return preg_match($regexp, $subject) == 1;
+    }
+
+    /**
+     * @param string $classname
+     * @return array
+     */
+    public static function classname(string $classname): string
+    {
+        return self::classnameSplit($classname)[1];
+    }
+
     /**
      * @param string $classname
      * @return array
@@ -99,26 +104,17 @@ final class Str
     }
 
     /**
-     * @param string $classname
-     * @return array
-     */
-    public static function classname(string $classname): string
-    {
-        return self::classnameSplit($classname)[1];
-    }
-    
-    /**
-     * Generate random filename 
+     * Generate random filename
      * @param string|null $ext
      * @param string|null $prefix
      * @return string
      */
     public static function filename(?string $ext = null, ?string $prefix = null): string
     {
-        return static::uID(7, $prefix) . ( $ext == null ? '' : '.' . ltrim($ext, '.'));
+        return static::uID(7, $prefix) . ($ext == null ? '' : '.' . ltrim($ext, '.'));
     }
-    
-     /**
+
+    /**
      * @param int $num
      * @param bool $useSymbols
      * @return string
@@ -127,106 +123,32 @@ final class Str
     {
         return ($prefix ?? '') . substr(bin2hex(random_bytes(ceil($num))), 0, $num);
     }
-    
-     /**
+
+    /**
      * @param int $num
      * @param bool $useSymbols
      * @return string
      */
     public static function pswd(int $num = 8, bool $useSymbols = true): string
     {
-        if ($useSymbols)
-        {
-            if ($num < 3)
-            {
+        if ($useSymbols) {
+            if ($num < 3) {
                 return static::random($num);
             }
-            
-            if ($num % 3 == 0)
-            {
+
+            if ($num % 3 == 0) {
                 return static::random($num = $num / 3, static::numbers) .
                     static::random($num, static::chars) .
                     static::random($num, static::symbols);
             }
-            
-            $pswd = static::random($multi = ($round = ceil($num / 3)) * 2, static::numbers . static::chars) 
+
+            $pswd = static::random($multi = ($round = ceil($num / 3)) * 2, static::numbers . static::chars)
                 . static::random($num - $multi, static::symbols);
-            
+
             return static::shuffle($pswd);
         }
-        
+
         return static::random($num, static::numbers . static::chars);
-    }
-    
-    /**
-     * @param string $content
-     * @return bool
-     */
-    public static function isJson(string $content): bool
-    {
-        return Json::isJson($content);
-    }
-
-    /**
-     * @param string $haystack
-     * @param string $needle
-     * @param bool $caseInsensitive
-     * @param int $offset
-     * @return bool
-     */
-    public static function contains(string $haystack, string $needle, bool $caseInsensitive = true, int $offset = 0): bool
-    {
-        try
-        {
-            return ($caseInsensitive ? mb_stripos($haystack, $needle, $offset) :
-                    mb_strpos($haystack, $needle, $offset)) !== false;
-        }
-
-        catch (\Throwable $e)
-        {
-            return false;
-        }
-    }
-    
-    /**
-     * @param string $haystack
-     * @param string[] $needle
-     * @param bool $caseInsensitive
-     * @param int $offset
-     * @return bool
-     */
-    public static function containsAny(string $haystack, array $needle, bool $caseInsensitive = true, int $offset = 0): bool
-    {
-        foreach ($needle as $item)
-        {
-            if (self::contains($haystack, $item, $caseInsensitive, $offset))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $haystack
-     * @param string $separator
-     * @param int $limit
-     * @return array
-     */
-    public static function explode(string $haystack, string $separator, int $limit = PHP_INT_MAX): array
-    {
-        return explode($separator, $haystack, $limit);
-    }
-    
-    /**
-     * @param string $haystack
-     * @param string $glue
-     * @return string
-     */
-    public static function implode(array $haystack, string $glue = ','): string
-    {
-        return implode($glue, $haystack);
     }
 
     /**
@@ -241,8 +163,7 @@ final class Str
 
         $string = '';
 
-        while ($num--)
-        {
+        while ($num--) {
             $string .= $chars[random_int(0, $max)];
         }
 
@@ -252,17 +173,72 @@ final class Str
     /**
      * @param string $string
      * @return string
-     * @throws \Exception
+     * @throws Exception
      */
     public static function shuffle(string $string): string
     {
         $chars = str_split($string, 1);
 
-        usort($chars, static fn (): int => ($left = random_int(0, 100)) == ($right = random_int(0, 100)) 
-              ? 0 : ($left > $right ? 1 : -1)
+        usort($chars, static fn(): int => ($left = random_int(0, 100)) == ($right = random_int(0, 100))
+            ? 0 : ($left > $right ? 1 : -1)
         );
 
         return implode('', $chars);
+    }
+
+    /**
+     * @param string $content
+     * @return bool
+     */
+    public static function isJson(string $content): bool
+    {
+        return Json::isJson($content);
+    }
+
+    /**
+     * @param string $haystack
+     * @param string[] $needle
+     * @param bool $caseInsensitive
+     * @param int $offset
+     * @return bool
+     */
+    public static function containsAny(string $haystack, array $needle, bool $caseInsensitive = true, int $offset = 0): bool
+    {
+        foreach ($needle as $item) {
+            if (self::contains($haystack, $item, $caseInsensitive, $offset)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $haystack
+     * @param string $needle
+     * @param bool $caseInsensitive
+     * @param int $offset
+     * @return bool
+     */
+    public static function contains(string $haystack, string $needle, bool $caseInsensitive = true, int $offset = 0): bool
+    {
+        try {
+            return ($caseInsensitive ? mb_stripos($haystack, $needle, $offset) :
+                    mb_strpos($haystack, $needle, $offset)) !== false;
+        } catch (Throwable $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param string $haystack
+     * @param string $separator
+     * @param int $limit
+     * @return array
+     */
+    public static function explode(string $haystack, string $separator, int $limit = PHP_INT_MAX): array
+    {
+        return explode($separator, $haystack, $limit);
     }
 
     /**
@@ -273,17 +249,26 @@ final class Str
      */
     public static function equalsAny(string $x, array $any, bool $caseInsensitive = true): bool
     {
-        foreach ($any as $string)
-        {
-            if (static::equals($x, (string) $string, $caseInsensitive))
-            {
+        foreach ($any as $string) {
+            if (static::equals($x, (string)$string, $caseInsensitive)) {
                 return true;
             }
         }
 
         return false;
     }
-    
+
+    /**
+     * @param string $x
+     * @param string $y
+     * @param bool $caseInsensitive
+     * @return bool
+     */
+    public static function equals(string $x, string $y, bool $caseInsensitive = true): bool
+    {
+        return $caseInsensitive ? strcasecmp($x, $y) == 0 : strcmp($x, $y) == 0;
+    }
+
     /**
      * @param string $subject
      * @param bool $multibyte
@@ -296,16 +281,6 @@ final class Str
     }
 
     /**
-     * @param string $regexp
-     * @param string $subject
-     * @return bool
-     */
-    public static function match(string $regexp, string $subject): bool
-    {
-        return preg_match($regexp, $subject) == 1;
-    }
-
-    /**
      * @param string $input
      * @param int $start
      * @param int $end
@@ -313,11 +288,19 @@ final class Str
      */
     public static function interval(string $input, int $start, int $end): string
     {
-        for ($string = ''; $end >= $start ; $start++)
-        {
+        for ($string = ''; $end >= $start; $start++) {
             $string .= $input[$start];
         }
 
         return $string;
+    }
+
+    /**
+     * @param int $length
+     * @return IString
+     */
+    public function slice(int $length): IString
+    {
+        return $this->substring($length);
     }
 }
