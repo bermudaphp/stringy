7be78d95367be26c51b503b6684c2f1bf05bead1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-23 23:33:49 +0200

    Update Jsonable.php

--- src/Jsonable.php
+++ src/Jsonable.php
@@ -6,8 +6,9 @@ interface Jsonable
 {
     /**
      * @param int $options
+     * @param int $depth
      * @return string
      * @throws \JsonException
      */
-    public function toJson(int $options = 0): string ;
+    public function toJson(int $options = 0, int $depth = 512): string ;
 }
